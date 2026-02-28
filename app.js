8b56d51eef3692b85f82a993fee5594a3d29c5b7
BeastEngineer <john@bethebeast.ai>
2026-02-28 15:44:30 -0500

    add project structure, mongoose connection to wtwr_db

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1/wtwr_db")
+  .then(() => console.log("connected to DB"))
+  .catch((err) => console.log("DB error", err));
+
 const express = require("express");
 
 const { PORT = 3001 } = process.env;
